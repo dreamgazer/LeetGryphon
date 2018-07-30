68566b8f3dbb1cd70001362acae4791c19a3834b
Shiqi Lian <lian.shiqi@wustl.edu>
2018-07-30 15:40:47 +0800

    solution for problem 10

--- Java/PalindromeNumber.java
+++ Java/PalindromeNumber.java
@@ -1,4 +1,21 @@
 
 public class PalindromeNumber {
-
+	public boolean isPalindrome(int x) {
+		if(x<0) return false;
+        int originNumber=x;
+		int reverseNumber=0;
+		while(x>0)
+		{
+			reverseNumber*=10;
+			reverseNumber+=x%10;
+			x/=10;
+		}
+		if(reverseNumber==originNumber)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
--- Java/Str2Int.java
+++ Java/Str2Int.java
@@ -1,4 +1,37 @@
+import java.util.ArrayDeque;
 
 public class Str2Int {
-
+	public int myAtoi(String str) {
+        if(str.length()==0) return 0;
+		int i=0;
+		int sign=1;
+		int number=0;
+		ArrayDeque<Integer> numbers=new ArrayDeque<Integer>();
+		while(i<str.length()&&str.charAt(i)==' '){
+			i++;
+		}
+		if(i>=str.length()||!(str.charAt(i)=='-'||str.charAt(i)=='+'||((str.charAt(i)<='9'&&str.charAt(i)>='0')))) return 0;
+		if(i<str.length()&&str.charAt(i)=='-')
+		{
+			i++;
+			sign=-1;
+		}
+		if(i<str.length()&&sign!=-1&&str.charAt(i)=='+') 
+		{
+			i++;
+			sign=1;
+		}
+		while(i<str.length()&&(str.charAt(i)<='9'&&str.charAt(i)>='0')) 
+		{
+			numbers.addLast(str.charAt(i++)-'0');
+		}
+		while(!numbers.isEmpty())
+		{
+			int digit=numbers.poll();
+			if(sign==1&&(double)(((double)number)*10+digit)>(double)Integer.MAX_VALUE) return Integer.MAX_VALUE;
+			if(sign==-1&&(double)(((double)number)*10+digit)*-1<(double)Integer.MIN_VALUE) return Integer.MIN_VALUE;
+			number=number*10+digit;
+		}
+		return number*sign;
+}
 }
