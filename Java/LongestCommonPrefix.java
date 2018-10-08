d722c29dbe7379dfa2f5a948c607952d346a8e0c
dreamgazer <574110878@qq.com>
2018-10-08 06:57:48 -0500

    problem 14

--- Java/LongestCommonPrefix.java
+++ Java/LongestCommonPrefix.java
@@ -4,25 +4,28 @@ public class LongestCommonPrefix {
 		if(strs.length==0) return "";
 		int min_length=Integer.MAX_VALUE;
         for(int i=0;i<strs.length;i++) {
-        	min_length=Math.min(min_length, strs[i].length());
+        	min_length=Math.min(min_length, strs[i].length()-1);
         }
         int upperbound=min_length;
-        int lowerbound=1;
-        while(lowerbound<upperbound) {
-            min_length=(upperbound+lowerbound)/2;
-        	if(isPrefix(min_length,strs)) {
-        		lowerbound=min_length+1;
-        	}
-        	else {
-        		upperbound=min_length-1;
-        	}
+        int lowerbound=0;
+	min_length = -1;
+        while(lowerbound <= upperbound) {
+        	int index = (lowerbound+upperbound)/2;
+		if(isPrefix(index, strs)){
+			min_length = index;
+			lowerbound = index + 1;
+		}
+		else{
+			upperbound = index - 1;
+		}
         	
         }
-        return strs[0].substring(0, min_length-1);
+	if (min_length == -1) return "";
+        return strs[0].substring(0, min_length+1);
     }
 	
 	private boolean isPrefix(int preLength,String[] strs) {
-		String preStr=strs[0].substring(0,preLength-1);
+		String preStr=strs[0].substring(0,preLength+1);
         for(int i=1;i<strs.length;i++) {
         	if(!strs[i].startsWith(preStr)) return false;
         }
